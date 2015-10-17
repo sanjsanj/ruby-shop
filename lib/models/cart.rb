6860cf525f6e74403c4aca1a371f15cab350ea00
sanjsanj <sanjsanj@hotmail.com>
2015-10-17 21:08:56 +0100

    shop can add multiples of a product to the cart

--- lib/models/cart.rb
+++ lib/models/cart.rb
@@ -6,11 +6,27 @@ class Cart
   end
 
   def add_item(id:)
-    item = (Products.find(id: id))
-    products << item
+    cart_contains_item?(id: id) ? increment_quantity(id: id) : add_to_cart(id: id)
+    p products
   end
 
   def total
     0
+  end
+
+  private
+
+  def cart_contains_item?(id:)
+    products.find { |product| product['id'] == id }
+  end
+
+  def increment_quantity(id:)
+    product = products.find { |product| product['id'] == id }
+    product['cart_quantity'] += 1
+  end
+
+  def add_to_cart(id:)
+    products << Products.find(id: id)
+    increment_quantity(id: id)
   end
 end
