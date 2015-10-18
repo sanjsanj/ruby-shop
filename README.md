154b385cdf7afc6fe5fc081cd5ab45364d6fa09d
sanjsanj <sanjsanj@hotmail.com>
2015-10-18 17:25:10 +0100

    features - user can add products to cart

--- README.md
+++ README.md
@@ -2,6 +2,3 @@
 
 ###Assumptions
 
-Products' shop_quantity will not decrement until items are actually purchased,
-since cart checkout has not been implemented this feature has not
-been developed yet.
--- spec/features/ruby_shop_feature_spec.rb
+++ spec/features/ruby_shop_feature_spec.rb
@@ -1,19 +1,37 @@
 feature 'Feature - The Ruby-Shop homepage' do
+  products = Products.data
+  item_1 = products.first
+  item_2 = products.last
+
   before do
     visit '/'
   end
 
+  after do
+    Capybara.reset!
+  end
+
   # As a User I can add a product to my shopping cart.
   scenario 'allows a product to be added to the cart' do
+    cart_total = item_1['price']
     all('.itemContainer')[0].click_on 'Buy'
-    expect(page).to have_css('.cartTotal', text: '99.00')
-    # text: sprintf('%.2f', cart_total)
+    expect(page).to have_css('.cartTotal', text:  sprintf('%.2f', cart_total))
   end
 
   scenario 'allows multiples of a product to be added to the cart' do
+    cart_total = item_1['price'] * 2
+    all('.itemContainer')[0].click_on 'Buy'
+    #visit '/'
+    #all('.itemContainer')[0].click_on 'Buy'
+    expect(page).to have_css('.cartTotal', text:  sprintf('%.2f', cart_total))
   end
 
   scenario 'allows multiples of multiple products to be added to the cart' do
+    cart_total =(item_1['price'] * 2) + (item_2['price'] * 2)
+    all('.itemContainer')[-1].click_on 'Buy'
+    visit '/'
+    all('.itemContainer')[-1].click_on 'Buy'
+    expect(page).to have_css('.cartTotal', text:  sprintf('%.2f', cart_total))
   end
 
   # As a User I am unable to add Out of Stock products to the shopping cart.
