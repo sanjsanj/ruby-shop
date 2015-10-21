399eee7cb17b8814ca29b52b615548946e887993
sanjsanj <sanjsanj@hotmail.com>
2015-10-21 15:39:30 +0100

    cart public interface tested

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 #Ruby-Shop
 
+[![Coverage Status](https://coveralls.io/repos/sanjsanj/ruby-shop/badge.svg?branch=master&service=github)](https://coveralls.io/github/sanjsanj/ruby-shop?branch=master)
+
 ###Assumptions
 
+###ToDo
+
+Why is my shop not resetting between scenarios.
--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -1,8 +1,19 @@
 describe Cart do
+  let(:cart) { Cart.new }
+  
   it 'initializes with empty products array' do
-    expect(subject.products).to eq []
+    expect(cart.products).to eq []
   end
   
-  it '' do
+  it 'can add an item to it' do
+    item = {"id" => 0, "cart_quantity" => 0}
+    cart.add_item(item: item)
+    expect(cart.products).to eq [item]
+  end
+
+  it 'knows the total price of products in it' do
+    item = { 'id' => 0, 'cart_quantity' => 0, 'price' => 99.00 }
+    cart.add_item(item: item)
+    expect(cart.total).to eq item['price']
   end
 end
